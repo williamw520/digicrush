2c9350522e3daf98c7384bef593176f58830df6d
William Wong <williamw520@gmail.com>
2020-08-05 00:04:17 -0700

    Add a simple test harness. The ES6 support from the current JS test frameworks sucks.

--- src/js/app.js
+++ src/js/app.js
@@ -9,26 +9,12 @@
 import logger from "/js/util/logger.js";
 import util from "/js/util/util.js";
 
-import {Vec2}  from "/js/pgengine/vec2.js";
-
 
 // app module
 let MD = (function() {
     const MD = { NAME: "app" };
     const L  = new logger.Logger(MD.NAME);
 
-    L.info(new Vec2(3, 4).len);
-    L.info(new Vec2(1, 1).unit);
-    L.info(new Vec2(1, 1).radian);
-    L.info(new Vec2(1, 2).radian);
-    L.info(new Vec2(-1, 2).radian);
-    L.info(new Vec2(-1, -2).radian);
-    L.info(new Vec2(-1, 9).radian);
-    L.info(new Vec2(1, 1).scale(2));
-
-    L.info(new Vec2(1, 1).asUnit());
-    L.info(new Vec2(1, 1).asUnit().asUnit());
-    
     L.info("module loaded");
     return MD;
 
---
+++ src/js/test.js
@@ -0,0 +1,30 @@
+/*
+  Digi Crush
+  A game to crush the digits.
+  Copyright (C) 2020 William Wong.  All rights reserved.
+  williamw520@gmail.com
+*/
+
+// The main entry point of unit tests.
+import logger from "./util/logger.js";
+import util from "/js/util/util.js";
+import {Vec2}  from "/js/pgengine/vec2.js";
+
+const L  = new logger.Logger("test");
+const A  = console.assert;
+
+L.info("Running tests");
+
+L.info(new Vec2(3, 4).len);
+L.info(new Vec2(1, 1).unit);
+L.info(new Vec2(1, 1).radian);
+L.info(new Vec2(1, 2).radian);
+L.info(new Vec2(-1, 2).radian);
+L.info(new Vec2(-1, -2).radian);
+L.info(new Vec2(-1, 9).radian);
+L.info(new Vec2(1, 1).scale(2));
+
+L.info(new Vec2(1, 1).asUnit());
+L.info(new Vec2(1, 1).asUnit().asUnit());
+
+
